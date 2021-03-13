f56c8d709a75446fa89c4dda2a459365f8a93d3f
Leonardo <leonardo.bongianni@gmail.com>
2021-03-13 10:46:20 +0100

    Ok js
    manca active

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  mouseEnterLeave();
+  menuPosition ();
+})
+
+
+
+// funzioni
+
+function mouseEnterLeave () {
+  $(".with-dropdown").mouseenter(function (){
+    $(this).children(".dropdown-menu").removeClass("dis-none");
+  })
+  $(".with-dropdown").mouseleave(function (){
+    $(this).children(".dropdown-menu").addClass("dis-none");
+  })
+  $(".dropdown-menu").mouseenter(function (){
+    $(this).removeClass("dis-none");
+  })
+  $(".dropdown-menu").mouseleave(function (){
+    $(this).addClass("dis-none");
+  })
+}
+
+function menuPosition () {
+  $(".with-dropdown").mouseenter(function (){
+    if ($(this).children(".dropdown-menu").hasClass("big")) {
+      $(this).children(".dropdown-menu").css("left", "-150px");
+    } else {
+      $(this).children(".dropdown-menu").css("left", "-30px");
+    }
+  })
+}
